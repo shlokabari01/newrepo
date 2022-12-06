e38df664de00822cc052e92c54513f6442ca6d1b
shloka 2 <shla1kor@bosch.com>
2022-12-06 17:31:48 +0530

    The program for addition of two numbers in java language

---
+++ assign2.java
@@ -0,0 +1,10 @@
+class main{
+        public static void main(String[] args) {
+                int a=10,b=20;
+
+
+
+               int c=a+b;
+                System.out.println("Sum of " + a + "and" + b + "is" c);
+        }
+}
